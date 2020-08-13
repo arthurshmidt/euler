14b2a0740a335469f44e6ca14e4149a2538a1c73
arthur <arthur@tartarus>
2020-08-12 23:58:09 -0400

    completed problem 4

--- problem4.py
+++ problem4.py
@@ -7,3 +7,13 @@
 
 if __name__ == "__main__":
     
+    max_val = 0
+    for i in range(100,1000,1):
+        for v in range(100,1000,1):
+            value = i * v
+            f_value = str(value)
+            r_value = f_value[::-1]
+            if (f_value == r_value) and (value > max_val):
+               max_val = value
+
+    print(max_val)
