2c9b0e260e31269c4ea43faa8178c2caf8cc9481
arthur <arthur@tartarus>
2020-08-18 23:58:06 -0400

    solved problem 7

--- problem7.py
+++ problem7.py
@@ -6,11 +6,21 @@
 # What is the 10001st prime number?
 
 if __name__ == "__main__":
-    
-    num_primes = 2
+    prime = False
+    num_primes = [2]
     counter = 3
-    while (True):
-        for i in range(1,counter):
-            if counter % i == 0 :
-               print(f"counter: {counter}, range: {i}")
+    while (len(num_primes) < 10001):
+        for i in range(2,counter):
+            if counter % i == 0:
+               # print(f"Not Prime - counter: {counter}, range: {i}")
+               prime = False
+               break
+            else:
+                # print(f"Prime - counter: {counter}, range: {i}")
+                prime = True
+        if prime == True:
+            num_primes.append(counter)
+            print(f"Prime: {counter}, Num Primes: {len(num_primes)}")
         counter += 1
+        prime = False
+
