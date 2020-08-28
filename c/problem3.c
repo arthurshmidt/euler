3c7701b7ce37837ebf3e40bef7538a763fe3862d
arthur <arthur@tartarus>
2020-08-27 23:26:55 -0400

    created problem 3 in C

---
+++ c/problem3.c
@@ -0,0 +1,19 @@
+/* the largest prime factor
+ *
+ * The prime factors for 13195 are 5, 7 13, and 29.
+ * 
+ * What is the largest prime factor for the number 600851475143
+ */
+
+ #include <stdio.h>
+
+ int main() {
+    int number = 0;
+    
+    printf("Enter number to factor:> ");
+    scanf("%d", number);
+
+    printf("Prime number = %d\n", number);
+    
+    return 0;
+ }
