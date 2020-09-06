b6666c6917ec7c0c268fa021a10ea2db1d9bd5fc
arthur <arthur@tartarus>
2020-09-05 23:02:09 -0400

    worked on problem 3 in C

--- C/problem3.c
+++ C/problem3.c
@@ -5,33 +5,54 @@
  * What is the largest prime factor for the number 600851475143
  */
 
+#define MAX_ARRAY 50
+
 #include <stdio.h>
 
 int largest_prime_factor(int number);
 
 int main() {
 	int number = 0;
-	int factor =0;
+	int factor = 0;
     
    	printf("Enter number to factor:> ");
-    	scanf("%d\n", number);
+    scanf("%d", &number);
 
-       	factor = largest_prime_factor(number);
+    factor = largest_prime_factor(number);
 	printf("The largest prime factor is: %d\n",factor);
 
-    	return 0;
+    return 0;
 }
 
 int largest_prime_factor(int number){
 	
 	int p_factor = 2;
 	int d_number = number;
+    int count = 0;
+    int p_factors[MAX_ARRAY] = {0};
 
 	while(d_number / p_factor != 1) {
-		if (d_number % p_factor == 0) 
+		if (d_number % p_factor == 0) { 
 			d_number = d_number / p_factor;
-		else if (d_number / p_factor > 0)
+            printf("1. d_number = %d, ",d_number);
+            printf("1. p_factor = %d, ",p_factor);
+            p_factors[count] = p_factor;
+        }
+		else if (d_number % p_factor > 0) {
 			p_factor += 1;
+            printf("2. d_number = %d, ",d_number);
+            printf("2. p_factor = %d, ",p_factor);
+        }
+        if (d_number / p_factor == 1) {
+            printf("3. d_number = %d, ",d_number);
+            printf("3. p_factor = %d  ",p_factor);
+            p_factors[count] = p_factor;
+        }
+        count += 1;
+        printf("\n");
 	}
+
+    for(int i=0; i < MAX_ARRAY; i++)
+        printf("Factor: %d\n",p_factors[i]);
 	return p_factor;	
 }
